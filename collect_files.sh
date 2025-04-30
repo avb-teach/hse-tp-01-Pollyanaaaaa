f799c10f124f02da952cec60aaddd3ba311f68e0
Polina Zueva <polinazueva@Noutbuk-Polina-3.local>
2025-04-30 21:36:34 +0300

    norm

--- collect_files.sh
+++ collect_files.sh
@@ -11,7 +11,7 @@ output_dir=$(realpath -m "$2")
 shift 2
 
 max_depth=""
-if [[ "$1" == "--max_depth" ]]; then
+if [[ $# -ge 2 && "$1" == "--max_depth" ]]; then
     max_depth="$2"
     shift 2
 fi
@@ -25,7 +25,10 @@ process_file() {
     local rel_path="${src_file#$input_dir/}"
     local base_name
     base_name=$(basename -- "$src_file")
-    local count=${name_counts["$base_name"]:-0}
+
+    # Глобальный счётчик по имени файла
+    local global_key="$base_name"
+    local count=${name_counts["$global_key"]:-0}
 
     if [[ "$base_name" =~ ^(.+)\.([^.]+)$ ]]; then
         local name=${BASH_REMATCH[1]}
@@ -40,22 +43,25 @@ process_file() {
         new_name="$base_name"
     else
         if [[ -n "$ext" ]]; then
-            new_name="${name}_${count}.${ext}"
+            new_name="${name}${count}.${ext}"
         else
-            new_name="${base_name}_${count}"
+            new_name="${name}${count}"
         fi
     fi
 
-    name_counts["$base_name"]=$((count + 1))
+    name_counts["$global_key"]=$((count + 1))
 
     if [[ -n "$max_depth" ]]; then
         IFS='/' read -ra parts <<< "$rel_path"
-        if (( ${#parts[@]} > max_depth )); then
-            return
+        local depth=${#parts[@]}
+
+        if (( depth > max_depth )); then
+            truncated_path="${parts[*]:depth - max_depth:max_depth}"
+            truncated_path="${truncated_path// /\/}"
+            dest_dir="$output_dir/${truncated_path%/*}"
+        else
+            dest_dir="$output_dir/${rel_path%/*}"
         fi
-        truncated_path="${parts[*]:0:${#parts[@]}-1}"
-        truncated_path="${truncated_path// /\/}"
-        dest_dir="$output_dir/$truncated_path"
     else
         dest_dir="$output_dir"
     fi
@@ -64,14 +70,8 @@ process_file() {
     cp -p "$src_file" "$dest_dir/$new_name"
 }
 
-if [[ -n "$max_depth" ]]; then
-    mapfile -d '' files < <(find "$input_dir" -mindepth 1 -maxdepth "$max_depth" -type f -print0)
-    for file in "${files[@]}"; do
-        process_file "$file"
-    done
-else
-    mapfile -d '' files < <(find "$input_dir" -type f -print0)
-    for file in "${files[@]}"; do
-        process_file "$file"
-    done
-fi
+mapfile -d '' files < <(find "$input_dir" -type f -print0)
+
+for file in "${files[@]}"; do
+    process_file "$file"
+done
